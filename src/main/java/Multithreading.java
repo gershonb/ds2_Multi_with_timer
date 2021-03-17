fb49940d9657a2519cd4491e833b35bed4d12dfa
gershonb <justformanchurian@gmail.com>
2021-03-17 14:34:13 -0400

    Spent crazy time on it. Very unclear. Sometimes contradictory and sometimes not even possible given the specs.

--- src/main/java/Multithreading.java
+++ src/main/java/Multithreading.java
@@ -1,6 +1,61 @@
-public class Multithreading {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-    public long listSum(int []list, int threads){
-        //code here
+public class Multithreading {
+    private static long results = 0;
+
+    public int[] listMaker() {
+        int[] myList = new int[100_000_000];
+        for (int i = 0; i < myList.length; i++) {
+            Random random = new Random();
+            myList[i] = random.nextInt(99) + 1;
+        }
+        return myList;
     }
+
+    static class myRunnable implements Runnable {
+        private int[] myList;
+        private int threadNumber = 1;
+        private int totalThreads = 1;
+        private long result = 0;
+
+        public int getTotalThreads() {
+            return totalThreads;
+        }
+
+        public myRunnable(int[] list, int ThreadNumber, int TotalThreads) {
+            this.threadNumber = ThreadNumber;
+            this.totalThreads = TotalThreads;
+            this.myList = list;
+        }
+
+        @Override
+        public void run() {
+            int start = ((myList.length / totalThreads) * (threadNumber -1));
+            int end = ((myList.length / totalThreads) * threadNumber) -1;
+            if(totalThreads == threadNumber){end = myList.length;}
+            for (int i = start; i < end; i++) {
+                results += i;
+            }
+        }
+
+
+    }
+
+    public static long listSum(int[] list, int threads) {
+        //code here
+
+        Thread[] tArray = new Thread[threads];
+        for (int i = 0; i < tArray.length; i++) {
+            Runnable target;
+            tArray[i] = new Thread(new myRunnable(list,i + 1, threads));
+        }
+        for (Thread thread : tArray) {
+            thread.start();
+        }
+        return results;
+    }
+
+
 }
--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -1,16 +1,87 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.IllegalFormatException;
+import java.util.Scanner;
+
 public class Timer {
+
 
     //DO NOT EDIT EXCEPT WITHIN METHODS
     
     public void start(){
         //code here
+
+        try {
+            File myObj = new File("startBool.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if(data.equals("1")){
+                    throw new IllegalArgumentException("Cannot start after a start without a reset");
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        String startTime = Long.toString(System.currentTimeMillis());
+        try {
+            FileWriter myWriter = new FileWriter("startTime.txt");
+            FileWriter myWriter2 = new FileWriter("startBool.txt");
+            myWriter.write(startTime);
+            myWriter2.write("1");
+            myWriter.close();
+            myWriter2.close();
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
     }
 
     public long stop(){
         //code here
+        long endTime = 0;
+        try {
+            File myObj = new File("startTime.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                endTime = Long.parseLong(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        try {
+            FileWriter myWriter2 = new FileWriter("startBool.txt", false);
+            myWriter2.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        return endTime;
     }
 
     public void reset(){
         //code here
+        try {
+            FileWriter myWriter = new FileWriter("startTime.txt", false);
+            FileWriter myWriter2 = new FileWriter("startBool.txt", false);
+            myWriter.close();
+            myWriter2.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
     }
 }
